6a60fc9e4a189b55bdbd219a7e6fe3cd9be86927
camiloperezII <camilomiguel.perezii@email.lcup.edu.ph>
2026-04-13 01:27:35 +0800

    Updates for the OJT_TRACKER
    added fixes and changes to the ojt_tracker

--- ojt-tracker-react/src/App.jsx
+++ ojt-tracker-react/src/App.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Admin from './pages/Admin' // Add this
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Admin from './pages/Admin';
+import Dashboard from './pages/Dashboard';
+import ForgotPassword from './pages/ForgotPassword';
+import UpdatePassword from './pages/UpdatePassword';
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<Admin />} /> {/* Add this */}
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/update-password" element={<UpdatePassword />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
